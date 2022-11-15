6a3892550656ec488eaf05bde084f32f995a22af
AhmadIbrahim99 <43531161+AhmadIbrahim99@users.noreply.github.com>
2022-11-15 17:02:09 +0200

    SetUp DataBase, Structure

---
+++ Aqary.DataAccessLayer/Models/AqaryDataBaseContext.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Aqary.DataAccessLayer.Models
+{
+    public partial class AqaryDataBaseContext : DbContext
+    {
+        public AqaryDataBaseContext(DbContextOptions<AqaryDataBaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public virtual DbSet<Attachment> Attachments { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Estate> Estates { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable("ApplicationUser");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasPrecision(0)
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.DeletedAt).HasPrecision(0);
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ImageString)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PasswordHash)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UpdatedAt).HasPrecision(0);
+            });
+
+            modelBuilder.Entity<Attachment>(entity =>
+            {
+                entity.ToTable("Attachment");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasPrecision(0)
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.DeletedAt).HasPrecision(0);
+
+                entity.Property(e => e.ImageString)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UpdatedAt).HasPrecision(0);
+
+                entity.HasOne(d => d.IdEstateNavigation)
+                    .WithMany(p => p.Attachments)
+                    .HasForeignKey(d => d.IdEstate)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Attachmen__IdEst__6754599E");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("Category");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasPrecision(0)
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.DeletedAt).HasPrecision(0);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UpdatedAt).HasPrecision(0);
+            });
+
+            modelBuilder.Entity<Estate>(entity =>
+            {
+                entity.ToTable("Estate");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasPrecision(0)
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.DeletedAt).HasPrecision(0);
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UpdatedAt).HasPrecision(0);
+
+                entity.HasOne(d => d.IdCategoryNavigation)
+                    .WithMany(p => p.Estates)
+                    .HasForeignKey(d => d.IdCategory)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Estate__IdCatego__6383C8BA");
+
+                entity.HasOne(d => d.IdUserNavigation)
+                    .WithMany(p => p.Estates)
+                    .HasForeignKey(d => d.IdUser)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Estate__IdUser__628FA481");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Aqary.DataAccessLayer/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Aqary.DataAccessLayer.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Estates = new HashSet<Estate>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual ICollection<Estate> Estates { get; set; }
+    }
+}
